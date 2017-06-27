299bc8112df755cef8865f03dffd356ec9791848
zhaoguangchao <zhaoguangchao@100tal.com>
2017-06-27 17:50:30 +0800

    init

---
+++ app/src/main/java/com/xes/yuedupractice/base/YueDuApplication.java
@@ -0,0 +1,21 @@
+package com.xes.yuedupractice.base;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ *     author : gooch
+ *     e-mail : zhaoguangchao@100tal.com
+ *     time   : 2017/06/27
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+
+public class YueDuApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
